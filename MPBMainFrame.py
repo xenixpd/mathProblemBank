16f7d6f75a755a564b23eb0e88efb75038c2ec21
xenixpd <43987629+xenixpd@users.noreply.github.com>
2018-10-17 12:33:53 +0900

    Add files via upload

---
+++ MPBMainFrame.py
@@ -0,0 +1,52 @@
+from tkinter import *
+
+def doNothing():
+	print("좋아요. 아주 좋아요")
+
+root = Tk()
+
+# 주메뉴(메뉴바)
+mainMenu = Menu(root)
+root.config(menu=mainMenu)
+
+# 메뉴 항목: 문제 등록
+
+# 먼저 주메뉴에 등록
+registerProblemMenu = Menu(mainMenu)
+mainMenu.add_cascade(label="문제 등록", menu=registerProblemMenu)
+
+# 메뉴 항목 추가
+registerProblemMenu.add_command(label="새 문제 등록...", command=doNothing)
+registerProblemMenu.add_command(label="New", command=doNothing)
+registerProblemMenu.add_separator()
+registerProblemMenu.add_command(label="종료", command=doNothing)
+
+# 메뉴 항목: 문제 검색
+
+# 먼저 주메뉴에 등록
+searchProblemMenu = Menu(mainMenu)
+mainMenu.add_cascade(label="문제 검색", menu=searchProblemMenu)
+
+# 메뉴 항목 추가
+searchProblemMenu.add_command(label="기본 검색", command=doNothing)
+searchProblemMenu.add_command(label="Cut", command=doNothing)
+searchProblemMenu.add_command(label="Paste", command=doNothing)
+
+
+# 주 도구바(툴바)
+mainToolbar = Frame(root, bg="blue")
+
+insertBtn = Button(mainToolbar, text="문제 등록", command=doNothing)
+insertBtn.pack(side=LEFT, padx=2, pady=2)
+
+printBtn = Button(mainToolbar, text="기본 검색", command=doNothing)
+printBtn.pack(side=LEFT, padx=2, pady=2)
+
+mainToolbar.pack(side=TOP, fill=X)
+
+# 주 상태바
+mainStatus = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
+mainStatus.pack(side=BOTTOM, fill=X)
+
+# tkinter 실행
+root.mainloop()
